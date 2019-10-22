23d54f33a14152aefb1741f7b93a8a85abc36cd4
Anish Bardhan <2020abardhan@dwight.edu>
2019-10-22 18:25:06 -0400

    first commit

---
+++ app/routes.py
@@ -0,0 +1,43 @@
+# importing necessary modules & functions from workload.py
+import collections
+import numpy as np
+import sys
+from flask import Flask
+from flask import render_template, request
+
+sys.path.append("/Users/2020abardhan/Desktop/workload/flaskproject-master/app/")
+import workload
+from workload import Order, Item, pretty_print_w, assign_time, order_orders, output_table
+
+# inits flask app w debugger on
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+@app.route('/base', methods=['GET'])
+def index():
+    out = ['', '', '', '', '']
+    return render_template('base.html', out=out)
+
+@app.route('/order', methods=['POST'])
+def order_now():
+    order1 = Order()
+    item1_1 = Item()
+    order1.id = request.form['order_id']
+    item1_1.type = request.form['order_item']
+    #day = request.form['day']
+    order1.items = [item1_1]
+
+    orders = [order1]
+    #get orders in order of their importance
+    ordered_orders = order_orders(orders)
+    #assign the orders to each day of the work week
+    work_week = assign_time(ordered_orders, 100)
+    #print out new schedule
+    out = output_table(work_week)
+
+    return render_template('base.html', out=out)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ run-variables.md
@@ -0,0 +1,5 @@
+export FLASK_APP=routes.py
+export FLASK_RUN_HOST=0.0.0.0
+export FLASK_RUN_PORT=8080
+export FLASK_DEBUG=1
+flask run
